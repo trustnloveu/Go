74a46609262cf70afa4915771d73abbdec050544
ejyang <trustnloveu@gmail.com>
2021-12-04 15:40:21 +0900

    initial commit with a template

---
+++ main.go
@@ -0,0 +1,19 @@
+/*
+	Cards
+
+	newDeck 		: Create a list of playing cards. Essentially an array of strings
+	print			: Log out the contents of a deck of cards
+	shuffle			: Shuffles all the cards in a deck
+	deal			: Create a 'hand' of cards
+	saveToFile		: Save a list of cards to a file on the local machine
+	newDeckFromFile	: Load a list of cards from the local machine
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("test")
+}
