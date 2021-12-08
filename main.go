c68be540e8be1af611689df091a8a3a1e62cd1c3
ejyang <trustnloveu@gmail.com>
2021-12-08 15:29:00 +0900

    Declare string variable, function

--- main.go
+++ main.go
@@ -15,5 +15,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("test")
+	// var card string = "Ace of Spades"
+	card := newCard()
+
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
