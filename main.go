9c66471c94c9e29fd004b5c0b72cf2db1d2dcaa7
ejyang <trustnloveu@gmail.com>
2021-12-11 20:58:22 +0900

    Declare person instance

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	// Not mapped
+	// austin := person{"Euijin", "Yang"}
 
+	// Mapped
+	austin := person{firstName: "Euijin", lastName: "Yang"}
+
+	fmt.Println(austin)
 }
